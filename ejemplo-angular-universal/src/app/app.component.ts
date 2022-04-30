cc06666397eeeb1d6837673b40b54b66cf3bd449
AngelVil92 <angelvil92@gmail.com>
2022-04-30 15:47:05 +0200

    Extra: ejemplo de hacer logout cuando pasa un tiempo sin inactividad en la aplicación

--- ejemplo-angular-universal/src/app/app.component.ts
+++ ejemplo-angular-universal/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { interval } from 'rxjs';
 import Swal from 'sweetalert2';
 import { AuthService } from './services/auth.service';
 
@@ -9,6 +10,8 @@ import { AuthService } from './services/auth.service';
 })
 export class AppComponent {
   isLoggedIn = false;
+  idTimeout: any = null
+  segundosInactivo: number = 0
 
   constructor(private auth: AuthService) {}
 
@@ -18,16 +21,42 @@ export class AppComponent {
     this.auth.logueado$
       .subscribe((logueado: boolean) => {
         this.isLoggedIn = logueado
+
+        if (logueado) {
+          // Si estamos logueados empezamos el contador de inactividad
+          this.inactividad()
+        }
       })
   }
 
   logout() {
     localStorage.removeItem('jwt')
     this.auth.logueado$.next(false)
-    Swal.fire("Logout")
   }
 
   cambiarVolumen(video: HTMLMediaElement, event: any) {
     video.volume = Number(event.target.value) / 100
+  }
+
+  @HostListener('window:click')
+  @HostListener('window:scroll')
+  @HostListener('window:mousemove')
+  incrementarInactividad() {
+    // Reseteamos los segundos de inactividad cuando se detecta un click sobre la página, que se mueve el ratón o que hacemos scroll
+    this.segundosInactivo = 0
+  }
+
+  inactividad() {
+    const subs = interval(1000)
+      .subscribe((n) => {
+        this.segundosInactivo += 1
+        console.log('Segundos inactividad', this.segundosInactivo)
+        // Al llegar a 5 segundos de inactividad hacemos el logout
+        if (this.segundosInactivo === 5) {
+          this.logout()
+          subs.unsubscribe()
+          Swal.fire("Se ha cerrado la sesión por inactividad")
+        }
+      })
   }
 }
